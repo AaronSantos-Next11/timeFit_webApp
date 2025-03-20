9dfad307c91cd449ddfe5bd941e894f9ad67746e
CesarSanchez19 <2302073@utrivieramaya.edu.mx>
2025-03-20 11:17:17 -0500

    add(Crear membresias y Crear servicio): añadir las vista de los componentes de los botones de crear servicio y membresia

---
+++ src/components/membership_management/memberships_and_services/CrearMembresia.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CrearMembresia = () => {
+  return (
+    <div>CrearMembresia</div>
+  )
+}
+
+export default CrearMembresia
---
+++ src/components/membership_management/memberships_and_services/CrearSerivicio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CrearSerivicio = () => {
+  return (
+    <div>CrearSerivicio</div>
+  )
+}
+
+export default CrearSerivicio
